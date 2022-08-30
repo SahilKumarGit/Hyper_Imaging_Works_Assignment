8efb3c278ba1b63098b4a18045cfd14a1c169b14
SahilKumarGit <email.sahil@yahoo.com>
2022-08-31 00:32:43 +0530

    backup-2022-08-31

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import supabase from "../operation/superbase"
+
+
+export default async function handler(req, res) {
+  let {data,error} = await supabase
+    .from('user')
+    .select('*')
+  res.status(200).json(data)
 }
+
